c718b8642de735d51d216d186fb26ca8afe44ecb
trcrsired <oyzawqgcfc@gmail.com>
2026-08-22 09:53:19 +0800

    [libherbceptions] implement itanium eh name/message

--- libherbceptions/src/__malloc_or_heap_alloc_temp_buffer.h
+++ libherbceptions/src/__malloc_or_heap_alloc_temp_buffer.h
@@ -28,21 +28,42 @@ inline void __free_or_heap_dealloc(void *__bufferptr) noexcept {
 #endif
 }
 
-class __malloc_or_heapalloc_temp_buffer {
+inline void *__malloc_or_die(::std::size_t __sz) noexcept {
+  void *__bufferptr = ::std::malloc(__sz);
+  if (__bufferptr == nullptr)
+    ::std::abort();
+  return __bufferptr;
+}
+
+template <bool __malloconly> class __basic_malloc_or_heapalloc_temp_buffer {
 public:
   void *__bufferptr{};
-  constexpr __malloc_or_heapalloc_temp_buffer() noexcept = default;
-  constexpr __malloc_or_heapalloc_temp_buffer(void *__bp) noexcept
+  constexpr __basic_malloc_or_heapalloc_temp_buffer() noexcept = default;
+  constexpr __basic_malloc_or_heapalloc_temp_buffer(void *__bp) noexcept
       : __bufferptr(__bp) {}
-  __malloc_or_heapalloc_temp_buffer(__malloc_or_heapalloc_temp_buffer const &) =
-      delete;
-  __malloc_or_heapalloc_temp_buffer &
-  operator=(__malloc_or_heapalloc_temp_buffer const &) = delete;
-  ~__malloc_or_heapalloc_temp_buffer() {
+  __basic_malloc_or_heapalloc_temp_buffer(
+      __basic_malloc_or_heapalloc_temp_buffer const &) = delete;
+  __basic_malloc_or_heapalloc_temp_buffer &
+  operator=(__basic_malloc_or_heapalloc_temp_buffer const &) = delete;
+  ~__basic_malloc_or_heapalloc_temp_buffer() {
     if (this->__bufferptr == nullptr)
       return;
-    ::std::error_domains::__herbceptions_detail::__free_or_heap_dealloc(
-        this->__bufferptr);
+    if constexpr (__malloconly) {
+      ::std::free(this->__bufferptr);
+    } else {
+      ::std::error_domains::__herbceptions_detail::__free_or_heap_dealloc(
+          this->__bufferptr);
+    }
   }
 };
+
+using __malloc_or_heapalloc_temp_buffer =
+    __basic_malloc_or_heapalloc_temp_buffer<false>;
+using __malloc_temp_buffer = __basic_malloc_or_heapalloc_temp_buffer<
+#ifdef _WIN32
+    true
+#else
+    false
+#endif
+    >;
 } // namespace std::error_domains::__herbceptions_detail
