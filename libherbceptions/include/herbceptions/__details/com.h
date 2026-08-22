142477c66464438b6dce5eb9046279edff2e0d32
trcrsired <oyzawqgcfc@gmail.com>
2026-08-22 18:21:51 +0800

    [libherbceptions] com,nt,win32 should have no errc
    value but just leave an errc there

--- libherbceptions/include/herbceptions/__details/com.h
+++ libherbceptions/include/herbceptions/__details/com.h
@@ -9,28 +9,15 @@ _WIN32/__CYGWIN__ targets.
 
 #if defined(_WIN32) || defined(__CYGWIN__)
 
+#include "herbceptions/error"
+
+#include <cstdint>
+
 namespace std {
 
 // The underlying type is uint32 so the standard 0x8000xxxx "failure" bit
 // pattern fits without narrowing.
-enum class com_errc : ::std::uint_least32_t {
-  ok = 0x00000000,
-  nointerface = 0x80004002,
-  fail = 0x80004005,
-  unexpected = 0x8000FFFF,
-  notimpl = 0x80004001,
-  outofmemory = 0x8007000E,
-  invalidarg = 0x80070057,
-  accessdenied = 0x80070005,
-  handle = 0x80070006,
-  abort = 0x80004004,
-  fail_ie = 0x8000FFFD,
-  pending = 0x8000000A,
-  cancelled = 0x800704C7,
-  notfound = 0x80070490,
-  alreadyexists = 0x800700B7,
-  nointerface_ie = 0x8000FFFE
-};
+enum class com_errc : ::std::uint_least32_t {};
 
 template <> class error_domain<::std::com_errc> {
 public:
--- libherbceptions/include/herbceptions/__details/nt.h
+++ libherbceptions/include/herbceptions/__details/nt.h
@@ -9,45 +9,13 @@ available on _WIN32/__CYGWIN__ targets.
 
 #if defined(_WIN32) || defined(__CYGWIN__)
 
+#include "herbceptions/error"
+
+#include <cstdint>
+
 namespace std {
 
-enum class nt_errc : ::std::uint_least32_t {
-  success = 0x00000000,
-  buffer_too_small = 0xC0000023,
-  access_denied = 0xC0000022,
-  invalid_handle = 0xC0000008,
-  invalid_parameter = 0xC000000D,
-  no_such_file = 0xC000000F,
-  end_of_file = 0xC0000011,
-  invalid_device_request = 0xC0000010,
-  more_processing_required = 0xC0000016,
-  insufficient_resources = 0xC000009A,
-  disk_full = 0xC000007F,
-  object_name_not_found = 0xC0000034,
-  object_path_not_found = 0xC000003A,
-  no_such_device = 0xC000000E,
-  device_not_ready = 0xC00000B5,
-  invalid_parameter_1 = 0xC00000EF,
-  invalid_parameter_2 = 0xC00000F0,
-  not_implemented = 0xC0000002,
-  internal_error = 0xC00000E5,
-  not_a_directory = 0xC0000103,
-  directory_not_empty = 0xC0000101,
-  object_name_invalid = 0xC0000033,
-  file_is_a_directory = 0xC00000BA,
-  privilege_not_held = 0xC0000061,
-  logon_failure = 0xC000006D,
-  account_restriction = 0xC000006E,
-  invalid_logon_hours = 0xC000006F,
-  invalid_workstation = 0xC0000070,
-  password_expired = 0xC0000071,
-  account_disabled = 0xC0000072,
-  too_many_links = 0xC0000096,
-  share_violation = 0xC0000043,
-  delete_pending = 0xC0000056,
-  file_locked = 0xC000005E,
-  too_many_opens = 0xC000011A
-};
+enum class nt_errc : ::std::uint_least32_t {};
 
 template <> class error_domain<::std::nt_errc> {
 public:
