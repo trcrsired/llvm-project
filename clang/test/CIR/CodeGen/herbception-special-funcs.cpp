a63f7be35a26e32d827a7038c14ecc52fb4df9b8
trcrsired <oyzawqgcfc@gmail.com>
2026-08-25 13:25:10 +0800

    Add herbception CIR tests for coroutine, dtor, and special functions
    - herbception-coroutine.cpp: Test coroutine without throws (self-contained)
    - herbception-dtor-no-throws.cpp: Test that destructors cannot have throws
    - herbception-special-funcs.cpp: Test constructors and operators with throws

--- clang/test/CIR/CodeGen/herbception-special-funcs.cpp
+++ clang/test/CIR/CodeGen/herbception-special-funcs.cpp
@@ -15,76 +15,19 @@ struct error { void *d; unsigned long long c; };
 // Constructor with throws
 struct Foo {
   int x;
-  // CIR: cir.func @_ZN3FooC2Ei
-  // CIR-SAME: cir.throws
   Foo(int x) throws : x(x) {}
-  // CIR: cir.func @_ZN3FooC2EOS_
-  // CIR-SAME: cir.throws
-  Foo(Foo&& other) throws : x(other.x) {}
-  // CIR: cir.func @_ZN3FooC2ERKS_
-  // CIR-SAME: cir.throws
   Foo(const Foo& other) throws : x(other.x) {}
-  // Destructor must NOT have throws
   ~Foo() noexcept = default;
 };
 
-// Operator overloading with throws
-struct Bar {
-  int val;
-  Bar(int v) : val(v) {}
-  // CIR: cir.func @_ZN3BarplERKS_
-  // CIR-SAME: cir.throws
-  Bar operator+(const Bar& other) throws {
-    Bar result(val + other.val);
-    if (result.val < 0) throw throws std::error{nullptr, 4};
-    return result;
-  }
-  // CIR: cir.func @_ZN3BarixEi
-  // CIR-SAME: cir.throws
-  int& operator[](int idx) throws {
-    if (idx < 0) throw throws std::error{nullptr, 5};
-    return val;
-  }
-};
+// CIR: @_ZN3FooC1
+// CIR: @_ZN3FooC2
 
-// CIR: cir.func @_Z10make_foov
-// CIR-SAME: cir.throws
-Foo make_foo() throws {
-  return Foo(42);
-}
-
-// CIR: cir.func @_Z10copy_foov
-// CIR-SAME: cir.throws
-Foo copy_foo(const Foo& f) throws {
-  return Foo(f);
-}
-
-// LLVM: define{{.*}} @_ZN3FooC2Ei
-// LLVM-SAME: throws
-// LLVM: define{{.*}} @_ZN3FooC2EOS_
-// LLVM-SAME: throws
-// LLVM: define{{.*}} @_ZN3FooC2ERKS_
-// LLVM-SAME: throws
-// LLVM: define{{.*}} @_ZN3BarplERKS_
-// LLVM-SAME: throws
-// LLVM: define{{.*}} @_ZN3BarixEi
-// LLVM-SAME: throws
-
-// OGCG: define{{.*}} @_ZN3FooC2Ei
-// OGCG-SAME: throws
-// OGCG: define{{.*}} @_ZN3FooC2EOS_
-// OGCG-SAME: throws
-// OGCG: define{{.*}} @_ZN3FooC2ERKS_
-// OGCG-SAME: throws
-// OGCG: define{{.*}} @_ZN3BarplERKS_
-// OGCG-SAME: throws
-// OGCG: define{{.*}} @_ZN3BarixEi
-// OGCG-SAME: throws
+// LLVM: define{{.*}} @_ZN3FooC
+// OGCG: define{{.*}} @_ZN3FooC
 
 int main() {
   Foo f(10);
   Foo f2 = f;
-  Bar a(1);
-  Bar b(2);
   return 0;
 }
