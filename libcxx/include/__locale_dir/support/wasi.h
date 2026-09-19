ea4b1120de3195eefec234459914b3563ea66d3e
trcrsired <oyzawqgcfc@gmail.com>
2026-09-20 04:46:24 +0800

    [libcxx] wasi has no definition of rune_table for locale
    Use the default table for wasi.h
    
    Try to resolve issue #223128
    
    [libcxx] we need to add wasi.h and default.h into the copy list
    
    [libcxx] support/default.h and support/wasi.h need to be in modulemap
    
    Or CI won't let us pass.
    
    [libcxx] default.h needs to be at the right order

---
+++ libcxx/include/__locale_dir/support/wasi.h
@@ -0,0 +1,17 @@
+//===-----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef _LIBCPP___LOCALE_DIR_SUPPORT_WASI_H
+#define _LIBCPP___LOCALE_DIR_SUPPORT_WASI_H
+
+#include <__locale_dir/support/default.h>
+#define _LIBCPP_PROVIDES_DEFAULT_RUNE_TABLE 1
+
+#include <__locale_dir/support/default/get_c_locale.h>
+
+#endif // _LIBCPP___LOCALE_DIR_SUPPORT_WASI_H
